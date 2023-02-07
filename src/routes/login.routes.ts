f0abf0ffdec6f8870ae840e3c42a81e65297e9d3
JoeAbiJaoudi <joemunji25@gmail.com>
2023-02-06 21:25:11 -0500

    Added login route/ made modifications to signup

---
+++ src/routes/login.routes.ts
@@ -0,0 +1,20 @@
+import express, { Express, Request, Response, Router } from 'express';
+import { verifyUser } from '../services/user.service';
+let router: Router = express.Router()
+
+//Verify user credentials against the database and login
+router.post('/login', async (req: Request, res: Response) =>  {
+  const { username, password } = req.body;
+
+  const isValid = await verifyUser(username, password);
+
+  if (isValid) {
+    res.status(200).json({ message: 'Login successful' });
+  } else {
+    res.status(401).json({ message: 'Incorrect username or password' });
+  }
+});
+
+module.exports = router
+
+
