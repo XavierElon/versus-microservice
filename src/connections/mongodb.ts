2b3e92335e0867b93f46c5620fc858a3c5fd2c2b
Xavier <xavierh93@hotmail.com>
2023-02-09 19:55:55 -0700

    eslint cleanup

--- src/connections/mongodb.ts
+++ src/connections/mongodb.ts
@@ -1,14 +1,16 @@
-import mongoDB, { Db, MongoClient } from 'mongodb'
+import { Db, MongoClient } from 'mongodb'
 import * as dotenv from 'dotenv'
 
 export const connectToDatabase = async () => {
-    dotenv.config()
-    console.log(process.env.ATLAS_URI)
-    const client: MongoClient = new MongoClient(process.env.ATLAS_URI!)
-    await client.connect()
+  dotenv.config()
+  /* eslint-disable no-console */
+  console.log(process.env.ATLAS_URI)
+  const client: MongoClient = new MongoClient(process.env.ATLAS_URI!)
+  await client.connect()
 
-    const db: Db = client.db(process.env.DB_NAME)
-    console.log(process.env.DB_NAME)
+  const db: Db = client.db(process.env.DB_NAME)
+  console.log(process.env.DB_NAME)
 
-    console.log(`Successfully connect to database: ${db.databaseName}`)
+  console.log(`Successfully connect to database: ${db.databaseName}`)
+  /* eslint-enable no-console */
 }
--- src/routes/login.routes.ts
+++ src/routes/login.routes.ts
@@ -1,20 +1,18 @@
-import express, { Express, Request, Response, Router } from 'express';
-import { verifyUser } from '../services/user.service';
+import express, { Request, Response, Router } from 'express'
+import { verifyUser } from '../services/user.service'
 const router: Router = express.Router()
 
 //Verify user credentials against the database and login
-router.post('/login', async (req: Request, res: Response) =>  {
-  const { username, password } = req.body;
+router.post('/login', async (req: Request, res: Response) => {
+  const { username, password } = req.body
 
-  const isValid = await verifyUser(username, password);
+  const isValid = await verifyUser(username, password)
 
   if (isValid) {
-    res.status(200).json({ message: 'Login successful' });
+    res.status(200).json({ message: 'Login successful' })
   } else {
-    res.status(401).json({ message: 'Incorrect username or password' });
+    res.status(401).json({ message: 'Incorrect username or password' })
   }
-});
+})
 
 module.exports = router
-
-
