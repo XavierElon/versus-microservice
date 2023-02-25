b86683feeb2537064eb3b49601a8c24b9011352f
JoeAbiJaoudi <joemunji25@gmail.com>
2023-02-25 08:31:08 -0500

    Works somtimes...

--- scripts/cron_delete_users.py
+++ scripts/cron_delete_users.py
@@ -1,41 +1,45 @@
 import asyncio
 import schedule
 import datetime
-import uuid
-import mongoengine
-from mongoengine import Document, StringField, IntField, BooleanField, DateTimeField
+from pymongo import MongoClient
 
-mongoengine.connect(db='users', host='mongodb+srv://root:IhvkxnDwROpiDZpd@jsx.nwqtn5o.mongodb.net', port=1017)
+# Establish a connection to the database
+client = MongoClient("mongodb+srv://root:IhvkxnDwROpiDZpd@jsx.nwqtn5o.mongodb.net/users", port=1017)
 
-class User(Document):
-    firstName = StringField(required=True)
-    lastName = StringField(required=True)
-    email = StringField(required=True)
-    mobileNumber = IntField(required=True)
-    userName = StringField(required=True)
-    password = StringField(required=True)
-    confirmationCode = StringField(default=uuid.uuid4().hex)
-    confirmationTokenExpirationTime = DateTimeField(null=True)
-    active = BooleanField(default=False)
-    date = DateTimeField(default=datetime.datetime.utcnow())
+# Get the database
+db = client['users']
+
+# Get the collection
+collection = db['users']
 
 async def deleteUnconfirmedUsers():
+   
     currentTime = datetime.datetime.now()
     print('SCRIPT IS RUNNING')
-    print(mongoengine.connection)
-    print(mongoengine)
 
-    # Calculate the timestamp for 24 hours ago
-    twentyFourHoursAgo = currentTime - datetime.timedelta(hours=24)
+    # Check if the connection is successful
+    if client is not None:
+        print(f"Connected to database '{db.name}'")
+    else:
+        print("Connection failed")
+    
+
+    # Calculate the timestamp for 2 minutes ago
+    twoMinutesAgo = currentTime - datetime.timedelta(minutes=2)
 
     # Find all users that are unconfirmed and have a confirmation token expiration time
-    # earlier than 24 hours ago
-    unconfirmedUsers = User.objects(active=False ) #confirmationTokenExpirationTime__lt=twentyFourHoursAgo
-    # Delete each unconfirmed user from the database
-    print(unconfirmedUsers)
-    for user in unconfirmedUsers:
-        await user.remove()
-
+    # earlier than 2 minutes ago
+    unconfirmedUsers = collection.find({'active': False, 'confirmationTokenExpirationTime': {'$lt': twoMinutesAgo}})
+    userCount = collection.count_documents({'active': False}) # count the number of unconfirmed users
+    print(f"Found {userCount} unconfirmed user(s).")
+    if userCount > 0:
+       for user in unconfirmedUsers:
+            print(user)
+            collection.delete_one({'_id': user['_id']})
+    else:
+      print("No unconfirmed user was found.")
+  
+    
 def run_delete_unconfirmed_users():
     asyncio.run(deleteUnconfirmedUsers())
 
