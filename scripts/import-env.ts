2767472118432153b9cd6aca65e2cd42d99820da
Xavier <xavierh93@hotmail.com>
2023-03-01 15:33:00 -0500

    update path for config

--- scripts/import-env.ts
+++ scripts/import-env.ts
@@ -13,7 +13,7 @@ import path from 'path';
 
 dotenv.config();
 
-const configPath = path.join(__dirname, '../src/config/config');
+const configPath = path.join(__dirname, '../src/config/config.ts');
 const configFile = fs.readFileSync(configPath, 'utf-8');
 const newConfigFile = configFile.replace(/process\.env\.[a-z]+(_[a-z]+)?/g, (match) => {
   const envVariable = match.split('.')[2];
