9b1ef8d306752c476958078db3aa47c8c49aa81e
Xavier <xavierh93@hotmail.com>
2023-03-08 17:41:31 -0500

    add ecr deploy script again

---
+++ scripts/ecr-deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 275136276893.dkr.ecr.us-west-2.amazonaws.com
+docker build -t xsj-users-microservice .
+docker tag xsj-users-microservice:latest 275136276893.dkr.ecr.us-west-2.amazonaws.com/xsj-users-microservice:latest
+docker push 275136276893.dkr.ecr.us-west-2.amazonaws.com/xsj-users-microservice:latest
