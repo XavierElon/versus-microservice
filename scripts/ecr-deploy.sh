f1e7ed4847b37d70d68a101461d9314872b27764
Xavier <xavierh93@hotmail.com>
2023-03-10 10:43:41 -0500

    update Dockerfile to remove cache

--- scripts/ecr-deploy.sh
+++ scripts/ecr-deploy.sh
@@ -3,4 +3,4 @@ aws ecr get-login-password --region us-west-2 | docker login --username AWS --pa
 docker build -t xsj-users-microservice .
 docker tag xsj-users-microservice:latest 275136276893.dkr.ecr.us-west-2.amazonaws.com/xsj-users-microservice:latest
 docker push 275136276893.dkr.ecr.us-west-2.amazonaws.com/xsj-users-microservice:latest
-#sudo aws ecs update-service --cluster xsj-users-microservice-cluster --service xsj-users-microservice-service --force-new-deployment --region us-west-2
+aws ecs update-service --cluster xsj-users-microservice-cluster --service xsj-users-microservice-service --force-new-deployment --region us-west-2
