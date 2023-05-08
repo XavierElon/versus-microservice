1861cebceea205fa45c9498322cf626b4e75002b
Xavier Hollingsworth <xavierelon93@gmail.com>
2023-05-08 13:15:44 -0400

    add credentials to cors

--- index.ts
+++ index.ts
@@ -29,6 +29,7 @@ app.use(express.json({ limit: '25mb' }))
 app.use(express.urlencoded({ extended: true, limit: '25mb' }))
 app.use(cors({
     origin: [`${FRONT_END_URL}`, 'https://xsj-3wwm1hfsm-xavierelon.vercel.app'],
+    credentials: true
 }))
 app.use(cookieParser())
 app.use(userRouter)
