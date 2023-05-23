ecfbfe54333a9e1b7450c25e1f7ff8d0e79575bd
Xavier <xavierh93@hotmail.com>
2023-05-23 11:54:14 -0400

    add convo for chatgpt

--- src/controllers/chatGPT.controllers.ts
+++ src/controllers/chatGPT.controllers.ts
@@ -1,17 +1,16 @@
 import { Request, Response } from 'express'
 import { openai } from '../..'
 
+let conversation = [{ role: 'system', content: 'You are a helpful assistant' }]
+
 export const ChatGPT = async (req: Request, res: Response): Promise<any> => {
-  console.log(req.body)
   const message: string = req.body.message
 
   try {
+    conversation.push({ role: 'user', content: message })
     const response = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
-      messages: [
-        { role: 'system', content: 'You are a helpful assistant.' },
-        { role: 'user', content: message }
-      ]
+      messages: conversation
     })
     const aiMessage = response.data.choices[0].message.content
     console.log(aiMessage)
