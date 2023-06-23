7afa8b632a68c0077c1454b7653ccf46f5206e9f
Xavier <xavierh93@hotmail.com>
2023-06-23 15:43:31 -0400

    converastion with chatgpt here

--- src/routes/chatgpt.routes.ts
+++ src/routes/chatgpt.routes.ts
@@ -1,9 +1,9 @@
 import express, { Router } from 'express'
 import { validateToken } from '../utils/jwt'
-import { CreateChatGPTConversation } from '../controllers/chatGPT.controllers'
+import { AddToChatGPTConversation } from '../controllers/chatGPT.controllers'
 
 export const chatGPTRouter: Router = express.Router()
 
-chatGPTRouter.post('/chatgpt', CreateChatGPTConversation)
+chatGPTRouter.post('/chatgpt', AddToChatGPTConversation)
 
 // chatGPTRouter.post('/chatGPT/:id/chat/:id', ChatGPTConverstation)
