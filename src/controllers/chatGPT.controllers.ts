c5ed4be335df75d3e9f99d9a2ed11897de7ca463
Xavier <xavierh93@hotmail.com>
2023-06-30 11:51:27 -0400

    test

--- src/controllers/chatGPT.controllers.ts
+++ src/controllers/chatGPT.controllers.ts
@@ -7,13 +7,10 @@ export const AddToChatGPTConversation3 = async (req: Request, res: Response): Pr
 
   let role
   let conversation = []
-  if (messages.legnth === 0) {
-    console.log('messages empty')
+  if (messages.length === 0) {
     messages.push({ role: 'system', content: 'You are a helpful assistant' })
   } else {
-    console.log('messages not empty')
     for (let i = 0; i < messages.length; i++) {
-      console.log(messages[i])
       if (messages[i].senderID !== 'chatGPT-3.5') {
         role = 'user'
       } else {
@@ -34,10 +31,10 @@ export const AddToChatGPTConversation3 = async (req: Request, res: Response): Pr
       model: 'gpt-3.5-turbo',
       messages: conversation
     })
-    console.log(response)
+
     const aiMessage = response.data.choices[0].message.content
-    console.log(aiMessage)
     conversation.push({ role: 'assistant', content: aiMessage })
+
     res.status(200).json({ message: aiMessage, conversation: conversation })
   } catch (error) {
     console.error(error)
@@ -45,46 +42,48 @@ export const AddToChatGPTConversation3 = async (req: Request, res: Response): Pr
   }
 }
 
-export const AddToChatGPTConversation4 = async (req: Request, res: Response): Promise<any> => {
-  const message: string = req.body.message
-  const messages: any = req.body.messages
+// Chat GPT 4 is in Beta and not accessible yet
+// export const AddToChatGPTConversation4 = async (req: Request, res: Response): Promise<any> => {
+//   const message: string = req.body.message
+//   const messages: any = req.body.messages
 
-  let role
-  let conversation = []
-  if (messages.legnth === 0) {
-    console.log('messages empty')
-    messages.push({ role: 'system', content: 'You are a helpful assistant' })
-  } else {
-    console.log('messages not empty')
-    for (let i = 0; i < messages.length; i++) {
-      console.log(messages[i])
-      if (messages[i].senderID !== 'chatGPT-4') {
-        role = 'user'
-      } else {
-        role = 'assistant'
-      }
-      let newMessageObject = {
-        role: role,
-        content: messages[i].text
-      }
-      conversation.push(newMessageObject)
-    }
-  }
+//   let role
+//   let conversation = []
+//   if (messages.length === 0) {
+//     console.log('messages empty')
+//     messages.push({ role: 'system', content: 'You are a helpful assistant' })
+//   } else {
+//     console.log('messages not empty')
+//     for (let i = 0; i < messages.length; i++) {
+//       console.log(messages[i])
+//       if (messages[i].senderID !== 'chatGPT-4') {
+//         role = 'user'
+//       } else {
+//         role = 'assistant'
+//       }
+//       let newMessageObject = {
+//         role: role,
+//         content: messages[i].text
+//       }
+//       conversation.push(newMessageObject)
+//     }
+//   }
+//   console.log('here')
+//   console.log(conversation)
+//   try {
+//     conversation.push({ role: 'user', content: message })
 
-  try {
-    conversation.push({ role: 'user', content: message })
-
-    const response = await openai.createChatCompletion({
-      model: 'gpt-4',
-      messages: conversation
-    })
-    console.log(response)
-    const aiMessage = response.data.choices[0].message.content
-    console.log(aiMessage)
-    conversation.push({ role: 'assistant', content: aiMessage })
-    res.status(200).json({ message: aiMessage, conversation: conversation })
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ error: 'An error occurred with ChatGPT API' })
-  }
-}
+//     const response = await openai.createChatCompletion({
+//       model: 'gpt-4',
+//       messages: conversation
+//     })
+//     console.log(response)
+//     const aiMessage = response.data.choices[0].message.content
+//     console.log(aiMessage)
+//     conversation.push({ role: 'assistant', content: aiMessage })
+//     res.status(200).json({ message: aiMessage, conversation: conversation })
+//   } catch (error) {
+//     console.error(error)
+//     res.status(500).json({ error: 'An error occurred with ChatGPT API' })
+//   }
+// }
--- src/routes/chatgpt.routes.ts
+++ src/routes/chatgpt.routes.ts
@@ -1,9 +1,9 @@
 import express, { Router } from 'express'
 import { validateToken } from '../utils/jwt'
-import { AddToChatGPTConversation3, AddToChatGPTConversation4 } from '../controllers/chatGPT.controllers'
+import { AddToChatGPTConversation3 } from '../controllers/chatGPT.controllers'
 
 export const chatGPTRouter: Router = express.Router()
 
 chatGPTRouter.post('/chatgpt3', AddToChatGPTConversation3)
 
-chatGPTRouter.post('/chatgpt4', AddToChatGPTConversation4)
+// chatGPTRouter.post('/chatgpt4', AddToChatGPTConversation4)
